82c713ec672f996b21a275bae6fafa0d6fa606f2
rpi11 <91921953+rpi11@users.noreply.github.com>
2023-07-27 00:42:40 -0400

    added label data read in

--- arabic/pca/consol_data.py
+++ arabic/pca/consol_data.py
@@ -9,6 +9,7 @@ import argparse
 import pandas as pd
 import numpy as np
 
+
 # reads in an ACLED csv file and converts it into a datafram
 # containing dates, total fatalities on that day, and the number of events per day
 def read_acled(path):
@@ -24,7 +25,11 @@ def read_acled(path):
         # groups the dataframe by event date and sums their fatalities and events
         ret_df = df.groupby(cols_interest, as_index=False, axis=0).agg({"fatalities":"sum","event_count":"sum"})
 
-        return ret_df
+    ret_df.columns = ["date","location","fatalities","event_count"]
+    ret_df["date"] = pd.to_datetime(ret_df["date"])
+    ret_df["location"] = df["location"].str.lower()
+
+    return ret_df
   
 def read_iom(file):
     with open(file, "r") as f:
@@ -77,21 +82,70 @@ def read_iom(file):
     
     new_df = pd.DataFrame(row_list, columns = df.columns)
     df = pd.concat([df,new_df]).sort_values(by=["STATE OF AFFECTED POPULATION","Date"]).reset_index().drop(columns=["index"])
+    df.columns = ["date","location","ipd"]
+    df["location"] = df["location"].str.lower()
 
     return df
+
+def read_labels(file, name_pop_df):
+    with open(file, "r") as f:
+        df = pd.read_csv(f)
+    df = df[["date","city"]+df.columns[6:].tolist()]
+
+    def replace_arabic(row, trans_dict):
+        city = row["city"]
+        if city in trans_dict:
+            city = trans_dict[city]
+        return city
+
+    city_translations = name_pop_df[["name_translated","name_en"]].set_index("name_translated")["name_en"].to_dict()
+    df["city"] = df.apply(replace_arabic, axis=1, args=(city_translations,))
+    
+    agg_dict = {}
+    for emotion in df.columns[2:]:
+        agg_dict[emotion] = "sum"
+    df = df.groupby(["date","city"], as_index=False, axis=0).agg(agg_dict)
+
+    for emotion in df.columns[2:]:
+        df[emotion] = (df[emotion] - df[emotion].mean()) / df[emotion].std()
+
+    df.rename(columns={"city":"location"}, inplace=True)
+    df["location"] = df["location"].str.lower()
+    df["date"] = pd.to_datetime(df["date"])
+
+    return df
+
+def merge_df(iom_df, df2, cols):
+    merged = pd.merge(iom_df, df2, on=cols, how="outer")
+    iom_locations = iom_df["location"].unique().tolist()
+    merged = merged[merged["location"].isin(iom_locations)]
+    merged = merged.fillna(0)
+    #merged.to_csv("out.csv", index=False)
+    
+    return merged
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-acled", default='data/acled.csv')
     parser.add_argument("-iom", default='data/iom.csv')
+    parser.add_argument("-labels", default="data/labels.csv")
+    parser.add_argument("-namePop", default="data/names-and-populations.csv")
     args = parser.parse_args()
-    
+
+    with open(args.namePop, "r") as f:
+        name_pop_df = pd.read_csv(f)
+
     # consolidate iom and acled data into smaller dataframes
     iom_df = read_iom(args.iom)
     acled_df = read_acled(args.acled)
+    label_df = read_labels(args.labels, name_pop_df)
 
+    #merge_df(iom_df, acled_df, ["date","location"])
     print(iom_df)
     print(acled_df)
+    print(label_df)
+
+    merged = merge_df(iom_df, label_df, ["date", "location"])
 
 
 if __name__ == "__main__":
