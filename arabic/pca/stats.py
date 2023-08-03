a400dab87b8d63fa0748bc5edad50ba33217cb1b
rpi11 <91921953+rpi11@users.noreply.github.com>
2023-08-03 01:46:07 -0400

    modified correlation method

--- arabic/pca/stats.py
+++ arabic/pca/stats.py
@@ -22,6 +22,7 @@ import warnings
 import statsmodels.api as sm
 
 from math import floor
+from tqdm import tqdm
 
 def regression(merged, week_offset, multiple=False):
     if type(merged) == str:
@@ -32,49 +33,75 @@ def regression(merged, week_offset, multiple=False):
         df = merged
 
     row_list = []
-    for loc in df["location"].unique():
+    for loc in tqdm(df["location"].unique(), desc="locations"):
         sub_df = df[df["location"]==loc]
         
-        iom_df = sub_df[["date","location","arriving_IDP","leaving_IDP"]]
-        indic_df = sub_df[[col for col in df.columns if col not in ["arriving_IDP","leaving_IDP"]]].copy()
+        IOM_df = sub_df[["date","location","arriving_IDP","leaving_IDP"]]
+        INDIC_df = sub_df[[col for col in df.columns if col not in ["arriving_IDP","leaving_IDP"]]].copy()
+        offset_list = list(range(-week_offset, week_offset))
 
-        for offset in range(-week_offset, week_offset):
-            indic_df["date"] += pd.Timedelta(weeks = offset)
-            merged_df = pd.merge(iom_df, indic_df, on=["date", "location"], how="inner")
+        if multiple:
+            offsets = []
+            non_iom_cols = [col for col in INDIC_df.columns if col not in IOM_df.columns]
+            for i in range(len(non_iom_cols)):
+                offsets.append(offset_list.copy()) 
+            offset_list = list(itertools.product(*offsets))
+
+        for offset in tqdm(offset_list, desc="offsets"):
+            if multiple:
+                indic_dfs = []
+                for col in non_iom_cols:
+                    indic_dfs.append(INDIC_df[["date","location", col]].copy())
+                indic_df = pd.DataFrame()
+                for i in range(len(offset)):
+                    indic_dfs[i]["date"] += pd.Timedelta(days = offset[i])
+                    if indic_df.empty:
+                        indic_df = indic_dfs[i]
+                    else:
+                        indic_df = pd.merge(indic_df, indic_dfs[i], on=["date","location"], how="inner")
+            else:
+                indic_df = INDIC_df.copy()
+                indic_df["date"] += pd.Timedelta(days = offset)
+            
+            merged_df = pd.merge(IOM_df, indic_df, on=["date", "location"], how="inner")
             merged_df.dropna()
 
             agg_dict = {}
             for col in indic_df.columns:
-                if col not in iom_df.columns:
+                if col not in IOM_df.columns:
                     agg_dict[col] = "sum"
             agg_dict["date"] = agg_dict["location"] = "first"
             agg_df = merged_df.groupby(["arriving_IDP","leaving_IDP"], as_index=False, axis=0).agg(agg_dict)
             
-            y_list = [col for col in iom_df.columns if col not in indic_df.columns]
-            x_list = [col for col in indic_df.columns if col not in iom_df.columns]
+            y_list = [col for col in IOM_df.columns if col not in indic_df.columns]
+            x_list = [col for col in indic_df.columns if col not in IOM_df.columns]
 
             for y in y_list:
                 Y = agg_df[y]
-
-                try:
+                if not (Y == 0.0).all():
                     if multiple:
                         X = agg_df[x_list]
                         lmfit = sm.OLS(Y, sm.add_constant(X)).fit()
                         r2 = lmfit.rsquared
-                        row_list.append([loc, y, x_list, len(x_list), r2, offset])
+                        if r2 >=0 and r2 <= 1:
+                            row_list.append([loc, y, x_list, len(x_list), r2, list(offset)])
                     else:
                         for x in x_list:
                             X = agg_df[x]
-                            lmfit = sm.OLS(Y, sm.add_constant(X)).fit()
-                            r2 = lmfit.rsquared
-                            row_list.append([loc, y, x, 1, r2, offset])
-                except ValueError:
-                    pass
-       
-    df_out = pd.DataFrame(row_list, columns = ["location","Y","X(s)","covariate num.","r^2","offset"])
+                            if not (X == 0.0).all():
+                                lmfit = sm.OLS(Y, sm.add_constant(X)).fit()
+                                r2 = lmfit.rsquared
+                                if r2 >=0 and r2 <= 1:
+                                    row_list.append([loc, y, x, 1, r2, offset])
+                # except ValueError:
+                #     pass
     
-    #df_out.to_csv(f"output/corr_iom_{merged_file.split('.')[0].split('_')[-1]}.csv", index=False)
-    return df_out.dropna()
+    df_out = pd.DataFrame(row_list, columns = ["location","Y","X(s)","covariate num.","r^2","offset"]).dropna()
+    
+    if type(merged) == str:
+        df_out.to_csv(f"output/corr_iom_{merged.split('.')[0].split('_')[-1]}.csv", index=False)
+    
+    return df_out
 
 def allstar_model(datafiles, week_offset):
     df = pd.DataFrame()
@@ -91,7 +118,7 @@ def allstar_model(datafiles, week_offset):
     df["date"] = pd.to_datetime(df["date"])
     df_out = pd.DataFrame()
     combos = list(itertools.product(*col_dict.values()))
-    for combo in combos:
+    for combo in tqdm(combos, desc="allstar configs"):
         reg_df = df[df.columns[:4].tolist()+list(combo)]
         if df_out.empty:
             df_out = regression(reg_df, week_offset, multiple=True)
@@ -113,10 +140,11 @@ def main():
     datafiles = args.__dict__
     
     allstar_model(datafiles, 4)
+    exit()
 
-    # for file in datafiles:''
-    #     regression(file)
-    
+    for file in datafiles:
+        regression(datafiles[file], 4)
+
 
 if __name__ == "__main__":
     main()
